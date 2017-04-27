09eb306f28e9245879c943c73b20e41c7ac89489
Andrey Seleznov <andrewaka39@gmail.com>
2017-04-27 21:26:27 +0100

    add numbers collection

---
+++ front_app/src/numbers_collection.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import './NumbersCollection.css';
+
+class NumbersCollection extends Component {
+  state = {
+    numbers: [],
+    page: 1,
+    per_page: 100
+  };
+
+  add_numbers_to_state = (element, index, array) => {
+    this.state.numbers.push(element);
+  };
+
+  update_per_page = (event) => {
+    this.setState({per_page: event.target.value});
+  }
+
+  view_page = (page, per_page) => {
+    if (page >= 1) {
+      var url = 'https://protected-wave-98335.herokuapp.com/numbers?page=' + page +'&per_page=' + per_page;
+      this.request_numbers(url);
+      this.setState({page: page});
+    }
+  }
+
+  request_numbers = (url) => {
+    var request = new Request(url, {
+      method: 'GET',
+      mode: 'cors'
+    });
+
+    fetch(request)
+        .then(response => response.json())
+        .then(json => {
+          this.setState({numbers: []})
+          json.forEach(this.add_numbers_to_state);
+          this.forceUpdate();
+        });
+  };
+
+  componentWillMount() {
+    var url = 'https://protected-wave-98335.herokuapp.com/numbers?page=1&per_page=100'
+    this.request_numbers(url);
+  }
+
+  render() {
+    var numbers = this.state.numbers.map(function(number) {
+      return (
+        <div>ddddd</div>
+      )}, this);
+
+    return(
+        <div className="NumbersCollection">
+          <div className="Numbers">
+            {numbers}
+          </div>
+          <div className="Navigation">
+            <div className="Previous" onClick={() => this.view_page(this.state.page - 1)}>&larr; PREVIOUS PAGE</div>
+            <div className="Next" onClick={() => this.view_page(this.state.page + 1)}>NEXT PAGE &rarr;</div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default NumbersCollection;
